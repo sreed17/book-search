756cc99e05fb9c9e1d7f8d0e10e8e2ae3bd77d90
Sreekanth D <inbox.sreekanthd@gmail.com>
2024-03-20 17:10:46 +0530

    chore: initialize react-query

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,18 @@ import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { AppContextProvider } from "./contexts/AppContext";
 import { RouterProvider } from "react-router-dom";
 import AppRouter from "./AppRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <AppContextProvider>
-        <RouterProvider router={AppRouter} />
-      </AppContextProvider>
-    </ThemeContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <AppContextProvider>
+          <RouterProvider router={AppRouter} />
+        </AppContextProvider>
+      </ThemeContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
